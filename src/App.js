c277ab6625fc54514a5cd528a453829a7cf96ec4
Alex Septefrati <shaptefratia@gmail.com>
2019-09-04 16:57:28 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My First PWA-Calculator</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
