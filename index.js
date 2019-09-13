3b64f1a294ed233d13c0230e850e00638706cb1a
Alex Septefrati <shaptefratia@gmail.com>
2019-09-13 12:56:19 +0300

    trying to fix again

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+//
+// app.use(express.static(path.join(__dirname, 'build')));
+//
+// app.get('/*', function(req, res) {
+// 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+//
+// // const PORT = process.env || 9000;
+// app.listen(9000);
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-const PORT = process.env || 9000;
-app.listen(PORT);
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+	request.addListener('end', function() {
+		file.serve(request, response);
+	}).resume();
+}).listen(process.env.PORT || 3000);
