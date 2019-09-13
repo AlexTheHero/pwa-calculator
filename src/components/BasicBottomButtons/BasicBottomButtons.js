5826a1a9615e52734293e34ccf0394afb0ddaa70
Alex Seven <shaptefratia@gmail.com>
2019-09-14 01:01:59 +0300

    Fixed issues with adding symbols and values[Extra cases]. Added functional for equal method.

--- src/components/BasicBottomButtons/BasicBottomButtons.js
+++ src/components/BasicBottomButtons/BasicBottomButtons.js
@@ -4,122 +4,113 @@ import "./_BasicBottomButtons.scss";
 import {ARITHMETIC_SYMBOLS, calculatePercentage, calculateValues, removeSignedValue} from "../../config/utils/utils";
 
 const onAddValue = (props, number) => {
-	if(props.displayValue && props.displayValue.length > 10){
-		return;
-	}
-	if(number === 0 && props.displayValue === "0"){
-		return;
-	}
-	else if(number === 0 && props.displayHistory === null && props.displayValue !== null){
-		return;
-	}
-	else if(number !== 0 && props.displayValue === "0" && number !== "."){
-		props.clearDisplayValue("displayValue");
-		props.addValue(number);
-		return;
-	}
-	
-	if(number === "."){
-		if(props.displayValue === null) return;
-		if(props.displayValue.includes(".")) return;
-	}
-	
-	props.addValue(number);
+  if (props.displayValue && props.displayValue.length > 10) {
+    return;
+  }
+  if (number === 0 && props.displayValue === "0") {
+    return;
+  } else if (number === 0 && props.displayHistory === null && props.displayValue !== null) {
+    return;
+  } else if (number !== 0 && props.displayValue === "0" && number !== ".") {
+    props.clearDisplayValue("displayValue");
+    props.addValue(number);
+    return;
+  }
+
+  if (number === ".") {
+    if (props.displayValue === null) return;
+    if (props.displayValue.includes(".")) return;
+  }
+
+  props.addValue(number);
 };
 
 const onAddSymbol = (props, symbol) => {
-	if(props.displaySymbol !== null && props.displayValue !== null){
-		props.makeHistory();
-		props.clearDisplayValue();
-		props.addSymbol(symbol);
-		return;
-	}
-	
-	if(props.displayValue !== null) {
-		props.makeHistory();
-		props.clearDisplayValue();
-	}
-	
-	if(props.displayHistory !== null && props.displaySymbol === null){
-		props.clearData();
-	}
-	
-	props.addSymbol(symbol);
+  if(props.displayValue === null) return;
+  if (props.displaySymbol !== null && props.displayHistory !== null) {
+    props.makeHistory();
+    props.clearDisplayValue();
+  }
+  else{
+    props.makeHistory();
+    props.clearDisplayValue();
+  }
+
+  props.addSymbol(symbol);
 };
 
 const onEqual = (props) => {
-	if(props.displayHistory === null || props.displaySymbol === null)return;
-	if(!props.displayHistory.match(ARITHMETIC_SYMBOLS) && props.displayValue === null) return;
-	
-	const valuesToProcess = props.displayValue !== null ? props.displayHistory + props.displaySymbol + props.displayValue : props.displayHistory;
-	let calculatedValue = calculateValues(valuesToProcess);
-	
-	if(props.displayValue !== null){
-		props.addHistory(`${props.displayValue} = ${calculatedValue}`);
-	}
-	else {
-		props.clearDisplayValue();
-		props.addHistory(` = ${calculatedValue}`);
-	}
-	
-	props.clearDisplayValue();
+  if (props.displayHistory === null || props.displaySymbol === null) return;
+  if (!props.displayHistory.match(ARITHMETIC_SYMBOLS) && props.displayValue === null) return;
+
+  const valuesToProcess = props.displayValue !== null ? props.displayHistory + props.displaySymbol + ' ' + props.displayValue : props.displayHistory;
+  let calculatedValue = calculateValues(valuesToProcess);
+
+  if (props.displayValue !== null) {
+    props.addHistory(`${props.displayValue} = ${calculatedValue}`);
+  } else {
+    props.clearDisplayValue();
+    props.addHistory(` = ${calculatedValue}`);
+  }
+
+  props.clearDisplayValue();
 };
 
 const onPercent = async (props) => {
-	if(props.displaySymbol !== null && props.displayHistory === null){
-		return;
-	}
-	if(props.displayValue === null){
-		return;
-	}
-	if(props.displaySymbol === null){
-		let newValue = removeSignedValue(props.displayValue);
-		let operatedPercent = calculatePercentage(Number(newValue));
-		props.clearData();
-		props.addHistory(`${newValue}% = ${operatedPercent}`);
-		
-		return;
-	}
-	
-	props.addHistory(`${props.displayValue}%`);
-	props.clearDisplayValue('displayValue');
+  if (props.displaySymbol !== null && props.displayHistory === null) {
+    return;
+  }
+  if (props.displayValue === null) {
+    return;
+  }
+  if (props.displaySymbol === null) {
+    let newValue = removeSignedValue(props.displayValue);
+    let operatedPercent = calculatePercentage(Number(newValue));
+    props.clearData();
+    props.addHistory(`${newValue}% = ${operatedPercent}`);
+
+    return;
+  }
+
+  props.addHistory(`${props.displayValue}%`);
+  props.clearDisplayValue('displayValue');
 };
 
 const BasicBottomButtons = ({props}) => {
-	return(
-		<div className="bottomContainer">
-			<div className="row">
-				<CalculatorButton handleClick={() => props.clearData()} text={"del"}/>
-				<CalculatorButton handleClick={() => props.makeSigned()} text={"+/-"}/>
-				<CalculatorButton handleClick={() => onPercent(props)} text={"%"}/>
-				<CalculatorButton handleClick={() => onAddSymbol(props, "/")} text={"/"}/>
-			</div>
-			<div className="row">
-				<CalculatorButton handleClick={() => onAddValue(props, 7)} text={"7"}/>
-				<CalculatorButton handleClick={() => onAddValue(props, 8)} text={"8"}/>
-				<CalculatorButton handleClick={() => onAddValue(props, 9)} text={"9"}/>
-				<CalculatorButton handleClick={() => onAddSymbol(props,"x")} text={"x"} customStyle/>
-			</div>
-			<div className="row">
-				<CalculatorButton handleClick={() => onAddValue(props, 4)} text={"4"}/>
-				<CalculatorButton handleClick={() => onAddValue(props, 5)} text={"5"}/>
-				<CalculatorButton handleClick={() => onAddValue(props, 6)} text={"6"}/>
-				<CalculatorButton handleClick={() => onAddSymbol(props,"-")} text={"-"} customStyle/>
-			</div>
-			<div className="row">
-				<CalculatorButton handleClick={() => onAddValue(props, 1)} text={"1"}/>
-				<CalculatorButton handleClick={() => onAddValue(props, 2)} text={"2"}/>
-				<CalculatorButton handleClick={() => onAddValue(props, 3)} text={"3"}/>
-				<CalculatorButton handleClick={() => onAddSymbol(props, "+")} text={"+"} customStyle/>
-			</div>
-			<div className="row">
-				<CalculatorButton handleClick={() => onAddValue(props, 0)} text={"0"}/>
-				<CalculatorButton isDisabled/>
-				<CalculatorButton handleClick={() => onAddValue(props,".")} text={","}/>
-				<CalculatorButton handleClick={() => onEqual(props)} text={"="} customStyle/>
-			</div>
-		</div>
-	)
+  return (
+    <div className="bottomContainer">
+      <div className="row">
+        <CalculatorButton handleClick={() => props.clearData()} text={"del"}/>
+        <CalculatorButton handleClick={() => props.makeSigned()} text={"+/-"}/>
+        <CalculatorButton handleClick={() => onPercent(props)} text={"%"}/>
+        <CalculatorButton handleClick={() => onAddSymbol(props, "/")} text={"/"}/>
+      </div>
+      <div className="row">
+        <CalculatorButton handleClick={() => onAddValue(props, 7)} text={"7"}/>
+        <CalculatorButton handleClick={() => onAddValue(props, 8)} text={"8"}/>
+        <CalculatorButton handleClick={() => onAddValue(props, 9)} text={"9"}/>
+        <CalculatorButton handleClick={() => onAddSymbol(props, "x")} text={"x"} customStyle/>
+      </div>
+      <div className="row">
+        <CalculatorButton handleClick={() => onAddValue(props, 4)} text={"4"}/>
+        <CalculatorButton handleClick={() => onAddValue(props, 5)} text={"5"}/>
+        <CalculatorButton handleClick={() => onAddValue(props, 6)} text={"6"}/>
+        <CalculatorButton handleClick={() => onAddSymbol(props, "-")} text={"-"} customStyle/>
+      </div>
+      <div className="row">
+        <CalculatorButton handleClick={() => onAddValue(props, 1)} text={"1"}/>
+        <CalculatorButton handleClick={() => onAddValue(props, 2)} text={"2"}/>
+        <CalculatorButton handleClick={() => onAddValue(props, 3)} text={"3"}/>
+        <CalculatorButton handleClick={() => onAddSymbol(props, "+")} text={"+"} customStyle/>
+      </div>
+      <div className="row">
+        <CalculatorButton handleClick={() => onAddValue(props, 0)} text={"0"}/>
+        <CalculatorButton isDisabled/>
+        <CalculatorButton handleClick={() => onAddValue(props, ".")} text={","}/>
+        <CalculatorButton handleClick={() => onEqual(props)} text={"="} customStyle/>
+      </div>
+    </div>
+  )
 };
 
 export default BasicBottomButtons;
--- src/config/utils/utils.js
+++ src/config/utils/utils.js
@@ -1,27 +1,72 @@
-export const ARITHMETIC_SYMBOLS = /([*+/-])/g;
+export const ARITHMETIC_SYMBOLS = /([x+/-])/g;
 
 export const calculatePercentage = (number, percentage = 1) => {
-	return (number / 100 * percentage)
+  return (number / 100 * percentage)
 };
 
 export const removeSignedValue = (value) => {
-	if(value[0] === '-'){
-		return value.substring(2);
-	}
-	
-	return value;
+  if (value[0] === '-') {
+    return value.substring(2);
+  }
+
+  return value;
+};
+
+export const resolvePercentageFromString = (values) => {
+  let newValues = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i].includes('%')) {
+      newValues.push(calculatePercentage(Number(values[i - 2]), Number(values[i].replace('%', ''))).toString());
+    } else {
+      newValues.push(values[i]);
+    }
+  }
+
+  return newValues;
+};
+
+export const resolveMultiplicationFromString = (values) => {
+  let newValues = [];
+  let i = 0;
+
+  while (i < values.length) {
+    if (values[i + 1] === '/') {
+      newValues.push(Number(values[i] / Number(values[i + 2])).toString());
+      i += 2;
+    } else if (values[i + 1] === 'x') {
+      newValues.push(Number(values[i] * Number(values[i + 2])).toString());
+      i += 2;
+    } else {
+      newValues.push(values[i]);
+    }
+    i++;
+  }
+
+  if (values.length === newValues.length) {
+    return newValues;
+  }
+
+  return resolveMultiplicationFromString(newValues);
+};
+
+export const resolveAdditionFromString = (values) => {
+  let sum = values.length === 1 ? values[0] : Number(values[0]);
+  let i = 0;
+  while (i < values.length) {
+    if (values[i] === '+') {
+      sum += Number(values[i + 1]);
+    } else if (values[i] === '-') {
+      sum -= Number(values[i + 1]);
+    }
+    i++;
+  }
+
+  return sum;
 };
 
 export const calculateValues = (values) => {
-	let theValues = values.replace(/\s+/g, '');
-	let finalValue = 0;
-	let counter = 0;
-	
-	for(let i = 0; i < theValues.length; i++){
-		if(theValues[i].match(ARITHMETIC_SYMBOLS)){
-			console.log("TODO!")
-		}
-	}
-	
-	return finalValue;
+  let theValues = values.trim().split(/\s+/);
+  theValues = resolveAdditionFromString(resolveMultiplicationFromString(resolvePercentageFromString(theValues)));
+
+  return theValues;
 };
